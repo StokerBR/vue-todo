52c2b6729dafa938b26c1e5a3ae7306c8de9bcd6
StokerBR <henriquemartinsc@gmail.com>
2023-03-07 04:45:22 +0000

    feat: Added toggle done and undone to ToDo items

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,16 +2,30 @@
     import {ref, onMounted} from 'vue';
     
     let todoItem = ref('');
-    let items = ref([
-        /* {name: 'a'},
-        {name: 'b'},
-        {name: 'c'}, */
-    ]);
+    let items = ref([]);
     
+    //Adds a new ToDo item if value is not empty
     function addTodoItem() {
         if (todoItem.value === '') return;
-        items.value.push({name: todoItem.value});
+        items.value.unshift({
+            name: todoItem.value,
+        });
         todoItem.value = '';
+    }
+    
+    //Toggles the checkbox and moves the item to the top or bottom of the list
+    function toggleCheck(index) {
+        let checked = !items.value[index]['checked']
+        let item = items.value.splice(index, 1)[0];
+        item.checked = checked;
+        
+        //If checked, move to bottom of list, otherwise move to top
+        if (checked) {
+            items.value.push(item);
+        } else {
+            items.value.unshift(item);
+        }
+        
     }
     
 </script>
@@ -23,13 +37,20 @@
         <div class="holder">
             
             <form @submit.prevent="addTodoItem">
-                <input type="text" placeholder="Add an item" v-model="todoItem">
+                <input type="text" class="add-item" placeholder="Add an item" v-model="todoItem">
             </form>
             
             <div class="list">
                 
                 <ul>
-                    <li v-for="(item, index) in items" :key="index">{{ item.name }}</li>
+                    <li v-for="(item, index) in items" :key="index" @click="toggleCheck(index)">
+                        
+                        <div class="checkbox-wrapper">
+                            <input :id="('checkbox-'+index)" type="checkbox" :checked="item.checked" @change="toggleCheck(index)">
+                            <label :for="('checkbox-'+index)">{{ item.name }}</label>
+                        </div>
+                        
+                    </li>
                 </ul>
                 
             </div>
@@ -44,8 +65,9 @@
 
 <style lang="scss" scoped>
     @import '@/assets/scss/variables.scss';
+    @import '@/assets/scss/checkbox.scss';
     
-    input {
+    input.add-item {
         width: calc(100% - 40px);
         border: 0;
         padding: 20px;
@@ -87,9 +109,13 @@
                 padding: 20px;
                 font-size: 1.3em;
                 background-color: $text-light;
-                border-left: 5px solid $indigo;
+                border-left: 5px solid $blue;
                 margin-bottom: 2px;
                 color: $white;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: flex-start;
             }
             
         }
